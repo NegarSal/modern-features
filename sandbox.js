dd29e56979fe4df195ff61c91d0d6a8c6baec5de
Negar <negarsaljooghi@gmail.com>
2024-05-09 17:02:59 +0330

    spread syntax (arrays)

--- sandbox.js
+++ sandbox.js
@@ -7,7 +7,9 @@ const result = double(1, 3, 5, 7, 9, 2, 4, 6, 8);
 console.log(result);
 
 // spread syntax (arrays)
-
-
+const people = ['shaun', 'ryu', 'crystal'];
+console.log(...people);
+const members = ['mario', 'chun-li', ...people];
+console.log(members);
 
 // spread syntax (objects)
