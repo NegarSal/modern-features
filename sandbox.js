e684e77e6f2d95746cb714ce11fdc234b3e8b12e
Negar <negarsaljooghi@gmail.com>
2024-05-09 16:55:10 +0330

    rest parameter

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,10 @@
 // rest parameter
-
+const double = (...nums) => {
+    console.log(nums);
+    return nums.map(num => num * 2);
+}
+const result = double(1, 3, 5, 7, 9, 2, 4, 6, 8);
+console.log(result);
 
 // spread syntax (arrays)
 
