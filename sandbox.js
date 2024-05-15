29a0423fdc162ddbb2bfdc96a5983f286844acaf
Negar <negarsaljooghi@gmail.com>
2024-05-15 18:40:36 +0330

    symbol

--- sandbox.js
+++ sandbox.js
@@ -1,33 +1,18 @@
-// sets
-const namesArray = ['ryu', 'chun-li', 'ryu', 'shaun'];
-console.log(namesArray);
+const symbolOne = Symbol('a generic name');
+const symbolTwo = Symbol('a generic name');
 
-//const namesSet = new Set(['ryu', 'chun-li', 'ryu', 'shaun']);
-const namesSet = new Set(namesArray);
-console.log(namesSet);
+console.log(symbolOne, symbolTwo, typeof symbolOne);
 
-//const uniqueNames = [...namesSet];
-const uniqueNames = [... new Set(namesArray)];
+console.log(symbolOne == symbolTwo);
+console.log(symbolOne === symbolTwo);
 
-console.log(uniqueNames);
+const ninja = {};
 
+ninja.age = 30;
+ninja['belt'] = 'orange';
+ninja['belt'] = 'black';
 
-const ages = new Set();
-// add elements to a set:
-ages.add(20);
-ages.add(25).add(30);
-// delete elements from a set:
-ages.delete(30);
-console.log(ages, ages.size);
-console.log(ages.has(30), ages.has(25));
-ages.clear();
-console.log(ages);
+ninja[symbolOne] = 'ryu';
+ninja[symbolTwo] = 'shaun';
 
-const ninjas = new Set([
-    { name: 'shaun', age: 30 },
-    { name: 'crystal', age: 29 },
-    { name: 'chunli', age: 32 },
-]);
-ninjas.forEach(ninja => {
-    console.log(ninja.name, ninja.age);
-});
+console.log(ninja);
